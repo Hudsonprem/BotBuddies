a7b4be94f7adf8fc7c07c5100b4465330a123904
Hudson Daniel <hudsonprem97@gmail.com>
2020-03-09 16:38:22 +0530

    Updates

---
+++ precache-manifest.8c4f965d391b68140ca084d75c126b1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eba0587f5248fa557da6ed42e9787fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c3f5434a87de82cea203",
+    "url": "/static/css/main.e9334af4.chunk.css"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/static/js/2.3a7757fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3a7757fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f5434a87de82cea203",
+    "url": "/static/js/main.d61e2dc1.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "ced7efea022e33735f463bf82feacef5",
+    "url": "/static/media/American Donuts.ced7efea.ttf"
+  }
+]);
