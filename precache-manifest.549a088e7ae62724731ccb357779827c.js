f2c25dc20e2e0d4e51f3d8cffc987c731c279d72
Hudson Daniel <hudsonprem97@gmail.com>
2020-03-10 19:01:06 +0530

    Updates

---
+++ precache-manifest.549a088e7ae62724731ccb357779827c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543850aa33ece2441ad7e891b7da23c6",
+    "url": "/BotBuddies/index.html"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/BotBuddies/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "761d366d75c04fc70d5c",
+    "url": "/BotBuddies/static/css/main.55fc9a69.chunk.css"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/BotBuddies/static/js/2.3a7757fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/BotBuddies/static/js/2.3a7757fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761d366d75c04fc70d5c",
+    "url": "/BotBuddies/static/js/main.ed2ef3a6.chunk.js"
+  },
+  {
+    "revision": "2138bf2dbd52f9bfdb53",
+    "url": "/BotBuddies/static/js/runtime-main.5786af29.js"
+  },
+  {
+    "revision": "ced7efea022e33735f463bf82feacef5",
+    "url": "/BotBuddies/static/media/American Donuts.ced7efea.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffe243a18f55d175a1ad6907cdfed1cf.js"
+  "/BotBuddies/precache-manifest.549a088e7ae62724731ccb357779827c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BotBuddies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
