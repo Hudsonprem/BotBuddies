0e78c9c776fe7b19d23c1522055ae838cb9f4f58
Hudson Daniel <hudsonprem97@gmail.com>
2020-05-09 16:01:27 +0530

    Updates

---
+++ precache-manifest.7723eb03ff0f12a5d10e216669a3bd1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9ab4ac64ca1c46c75544574425224c",
+    "url": "/pokecards/index.html"
+  },
+  {
+    "revision": "c562bca94c5b687a71f7",
+    "url": "/pokecards/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2795cfc3992ee34907df",
+    "url": "/pokecards/static/css/main.f3a7010a.chunk.css"
+  },
+  {
+    "revision": "c562bca94c5b687a71f7",
+    "url": "/pokecards/static/js/2.40d9d520.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokecards/static/js/2.40d9d520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2795cfc3992ee34907df",
+    "url": "/pokecards/static/js/main.8b61721a.chunk.js"
+  },
+  {
+    "revision": "5059ed9f35664b259980",
+    "url": "/pokecards/static/js/runtime-main.8b9b7c75.js"
+  },
+  {
+    "revision": "c5e7d25b3bb0f11d8689530e2e4c5d14",
+    "url": "/pokecards/static/media/EridanusCondensed3DItalic-1gXL.c5e7d25b.otf"
+  },
+  {
+    "revision": "a50e9724220f5182022964fc97251ead",
+    "url": "/pokecards/static/media/OverstreetBible-Mnmw.a50e9724.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BotBuddies/precache-manifest.3cf6facfba4a2ea887a530cced00f5aa.js"
+  "/pokecards/precache-manifest.7723eb03ff0f12a5d10e216669a3bd1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BotBuddies/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokecards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
